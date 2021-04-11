eecda11cd69d06be9643fe6f49dd7e440242f059
Tomi Tyrrell <61401784+tomityrrell@users.noreply.github.com>
2021-04-11 15:40:13 -0400

    Weather Report Analysis

--- loaders/weather_station/weather_reports.py
+++ loaders/weather_station/weather_reports.py
@@ -3,27 +3,36 @@ import json
 
 import pandas as pd
 
-with open('cimis_api_key', 'r') as f:
-    appKey = f.readline()
+with open('./loaders/weather_station/cimis_api_key', 'r') as f:
+    appKey = f.readline().strip()
 
-stations = "6"
+# stations = "6"
 # stations = "6,  28,  58,  74,  98, 198, 217, 238, 250, 251, 255"
-startDate = "2021-01-12"
-endDate = "2021-01-21"
+# startDate = "2021-01-12"
+# endDate = "2021-01-21"
 
-address = "https://et.water.ca.gov/api/data?appKey={}&targets={}&startDate={}&endDate={}&dataItems=hly-air-tmp,hly-dew-pnt,hly-eto,hly-net-rad,hly-asce-eto,hly-asce-etr,hly-precip,hly-rel-hum,hly-res-wind,hly-soil-tmp,hly-sol-rad,hly-vap-pres,hly-wind-dir,hly-wind-spd&unitOfMeasure=E"
+address = "http://et.water.ca.gov/api/data?appKey={}&targets={}&startDate={}&endDate={}&dataItems=hly-air-tmp,hly-dew-pnt,hly-eto,hly-net-rad,hly-asce-eto,hly-asce-etr,hly-precip,hly-rel-hum,hly-res-wind,hly-soil-tmp,hly-sol-rad,hly-vap-pres,hly-wind-dir,hly-wind-spd&unitOfMeasure=E"
 
-r = requests.get(address.format(appKey, stations, startDate, endDate))
-j = json.loads(r.content)
 
-data = j["Data"]["Providers"]
-data_df = pd.DataFrame(data)
+def weather_station_query(queries, save=False):
+    query_dfs = []
+    for (station, startDate, endDate) in queries:
+        r = requests.get(address.format(appKey, station, startDate, endDate))
+        j = json.loads(r.content)
 
-hly_cols = data_df.columns[data_df.columns.str.contains("Hly")]
-hly_sample = data_df.iloc[0][hly_cols]
-hly_cols_units = map(lambda i: i[0] + " " + i[1], hly_sample.apply(lambda r: r["Unit"]).items())
+        query = j["Data"]["Providers"][0]["Records"]
+        query_dfs.append(pd.DataFrame(query))
 
-data_df[hly_cols] = data_df[hly_cols].applymap(lambda r: r["Value"])
-data_df.rename(columns={c: cu for (c, cu) in zip(hly_cols, hly_cols_units)}, inplace=True)
+    data_df = pd.concat(query_dfs)
 
-data_df.to_csv("./data/weather/weather_report.csv", index=False)
+    hly_cols = data_df.columns[data_df.columns.str.contains("Hly")]
+    hly_sample = data_df.iloc[0][hly_cols]
+    hly_cols_units = map(lambda i: i[0] + " " + i[1], hly_sample.apply(lambda r: r["Unit"]).items())
+
+    data_df[hly_cols] = data_df[hly_cols].applymap(lambda r: r["Value"])
+    data_df.rename(columns={c: cu for (c, cu) in zip(hly_cols, hly_cols_units)}, inplace=True)
+
+    if save:
+        data_df.to_csv("./data/weather/weather_report_{}.csv".format(str(queries)), index=False)
+
+    return data_df
